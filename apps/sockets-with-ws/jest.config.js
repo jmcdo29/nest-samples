113f944f652cd1e29805f0d5a8007af14b683715
Jay McDoniel <jmcdo29@gmail.com>
2021-06-18 12:38:56 -0700

    initial commit

---
+++ apps/sockets-with-ws/jest.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  displayName: 'sockets-with-ws',
+  preset: '../../jest.preset.js',
+  globals: {
+    'ts-jest': {
+      tsconfig: '<rootDir>/tsconfig.spec.json',
+    },
+  },
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.[tj]s$': 'ts-jest',
+  },
+  moduleFileExtensions: ['ts', 'js', 'html'],
+  coverageDirectory: '../../coverage/apps/sockets-with-ws',
+};
