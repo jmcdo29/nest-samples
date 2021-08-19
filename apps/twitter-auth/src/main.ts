472bfcaf9f0864d834623081995b829aae70f9ff
ProductOfAmerica <jeff.smith34r32@gmail.com>
2021-08-18 20:33:25 -0500

    Whoops, forgot sessions. Added them.

--- apps/twitter-auth/src/main.ts
+++ apps/twitter-auth/src/main.ts
@@ -2,10 +2,10 @@
  * This is not a production server yet!
  * This is only a minimal backend to get started.
  */
-
+import * as exsession from 'express-session';
 import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
-
+import { initialize, session } from 'passport';
 import { AppModule } from './app/app.module';
 
 async function bootstrap() {
@@ -13,6 +13,17 @@ async function bootstrap() {
   const globalPrefix = 'api';
   app.setGlobalPrefix(globalPrefix);
   const port = process.env.PORT || 3333;
+  app.use(
+    exsession({
+      // TODO change this for production
+      cookie: { secure: false },
+      secret: 'YOUR_SECRET_MUST_CHANGE_THIS_OR_USE_CONFIG_SERIVCE',
+      resave: true,
+      saveUninitialized: true,
+    }),
+  );
+  app.use(initialize());
+  app.use(session());
   await app.listen(port, () => {
     Logger.log('Listening at http://localhost:' + port + '/' + globalPrefix);
   });
